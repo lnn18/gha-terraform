56b11124d27ded803548f41422814d0c9526adc7
LinaFigueredo <linis9418@gmail.com>
2025-12-09 12:03:41 -0500

    add workflow tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      version = ">= 5.52.0"
+    }
+  }
+  required_version = ">= 1.1.9"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "fer-gha-tf-bucket" {
+  bucket = "244190102671-lina-figueredo"
+}
